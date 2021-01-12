e85c8da55bf2e7c345e74841c2dfa0c73f4e9003
Kamil Samigullin <kamil@samigullin.info>
2021-01-12 22:18:02 +0300

    issue #1 and #2: define base types

--- middleware.go
+++ middleware.go
@@ -1,1 +1,11 @@
 package propaganda
+
+import "net/http"
+
+// The Input represents a middleware for a http.Handler.
+//
+//  server := http.Server{
+//  	Handler: Input1(Input2(http.DefaultServeMux)),
+//  }
+//
+type Input = func(http.Handler) http.Handler
--- tripper.go
+++ tripper.go
@@ -1,1 +1,11 @@
 package propaganda
+
+import "net/http"
+
+// The Output represents a middleware for a http.RoundTripper.
+//
+//  client := http.Client{
+//  	Transport: Output1(Output2(http.DefaultTransport)),
+//  }
+//
+type Output = func(http.RoundTripper) http.RoundTripper
